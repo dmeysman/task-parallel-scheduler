5b64d8b7fd9384e5a2b16c174fd55d3edb3700ce
Dylan Meysmans <dmeysman@vub.ac.be>
2015-08-12 21:32:31 +0200

    Describe project

---
+++ pack.pl
@@ -0,0 +1,8 @@
+name(task_parallel_scheduler).
+title('Task-Parallel Scheduler').
+version('0.1.0').
+author('Dylan Meysmans', 'dmeysman@vub.ac.be').
+maintainer('Dylan Meysmans', 'dmeysman@vub.ac.be').
+packager('Dylan Meysmans', 'dmeysman@vub.ac.be').
+home('https://ai.vub.ac.be/node/1353').
+download('https://github.com/dmeysman/task-parallel-scheduler/release/*.zip').
