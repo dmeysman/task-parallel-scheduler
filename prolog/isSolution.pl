58c8587641cec38a390b1f80d21fc265897f3b91
Dylan Meysmans <dmeysman@vub.ac.be>
2015-08-15 23:26:01 +0200

    Implement dependency relation for tasks

--- prolog/isSolution.pl
+++ prolog/isSolution.pl
@@ -6,11 +6,11 @@
 */
 
 :- module(isSolution, [isSolution/1,
-                       cores/1,
-                       tasks/1,
-                       test_schedules/3,
-                       ordered_tasks/1,
-                       select_many/3]).
+                       generate_schedules/3,
+                       ordered_tasks/2,
+                       depends_on_any/2,
+                       depends_on/2,
+                       selectchk_many/3]).
 
 :- use_module(library(lists)).
 
@@ -20,43 +20,66 @@
 %! isSolution(+S:solution) is semidet.
 %! isSolution(-S:solution) is nondet.
 %
-% Succeeds if S is a solution to the scheduling problem, not taking into account dependencies.
-% This is necessary for finding all solutions to task scheduling problem instances with independent tasks.
+% Succeeds if S is a solution to the scheduling problem, accounting for dependencies.
 % You need not instantiate S, if you do not, it is instantiated to such a solution.
 isSolution(solution(Ss)) :-
   cores(Cs),
   tasks(Ts),
-  test_schedules(Cs, Ts, Ss).
+  generate_schedules(Cs, Ts, Ss).
 
-%! test_schedules(+Cs:list, +Ts:list, +Ss:list) is semidet.
-%! test_schedules(+Cs:list, +Ts:list, -Ss:list) is nondet.
+%! generate_schedules(+Cs:list, +Ts:list, +Ss:list) is semidet.
+%! generate_schedules(+Cs:list, +Ts:list, -Ss:list) is nondet.
 %
-% Succeeds if Ss is a list of schedules using Cs and Ts.
+% Succeeds if Ss is a list of schedules using Cs and Ts, accounting for dependencies.
 % You need not instantiate Ss, if you do not, it is instantiated to such a list of schedules.
-test_schedules([], [], []).
-test_schedules([C|Cs], Ts, [schedule(C,Us)|Ss]) :-
-  ordered_tasks(Us),
-  select_many(Us, Ts, Vs),
-  test_schedules(Cs, Vs, Ss).
+generate_schedules([], [], []).
+generate_schedules([C|Cs], Ts, [schedule(C,Us)|Ss]) :-
+  ordered_tasks(Us, Ts),
+  selectchk_many(Us, Ts, Vs),
+  generate_schedules(Cs, Vs, Ss).
 
-%! ordered_tasks(+Ts:list) is semidet.
-%! ordered_tasks(-Ts:list) is nondet.
+%! ordered_tasks(+Ts:list, +Us:list) is semidet.
+%! ordered_tasks(+Ts:list, -Us:list) is nondet.
 %
-% Succeeds if Ts is a list of tasks which satisfy @</2 pairwise.
-% You need not instantiate Ts, if you do not, it is instantiated to such a list of tasks.
-ordered_tasks([]).
-ordered_tasks([_|[]]).
-ordered_tasks([T|[U|Ts]]) :-
-  user:task(T),
-  user:task(U),
-  T @< U,
-  ordered_tasks([U|Ts]).
+% Succeeds if Us is a subset of tasks of Ts which respect the reflexive and transitive closure
+% of the dependency relation. You need not instantiate Ts, if you do not,
+% it is instantiated to such a list of tasks.
+ordered_tasks([], _).
+ordered_tasks([T|Ts], Us) :-
+  select(T, Us, Vs),
+  ordered_tasks(Ts, Vs),
+  not(depends_on_any(T, Ts)).
 
-%! select_many(+Ys:list, +Xs:list, -Zs:list) is semidet.
+%! depends_on_any(+T, +Ds:list) is semidet.
+%
+% Succeeds if T directly or indirectly depends on any element of Ds.
+depends_on_any(_, []) :-
+  fail.
+depends_on_any(T, [D|_]) :-
+  depends_on(T, D).
+depends_on_any(T, [D|Ds]) :-
+  not(depends_on(T, D)),
+  depends_on_any(T, Ds).
+
+%! depends_on(+T, +D) is semidet.
+%
+% Succeeds if T directly or indirectly depends on D.
+% Represents the reflexive and transitive closure of the dependency relation.
+depends_on(T, T).
+depends_on(T, D) :-
+  T \== D,
+  user:depends_on(T, D, _).
+depends_on(T, D) :-
+  T \== D,
+  not(user:depends_on(T, D, _)),
+  user:depends_on(T, V, _),
+  depends_on(V, D).
+
+%! selectchk_many(+Ys:list, +Xs:list, -Zs:list) is semidet.
 %
 % Instantiates Zs to Xs with all first occurences of elements of Ys removed.
 % Fails if an element of Ys does not occur in Xs or when Xs is [].
-select_many([], Xs, Xs).
-select_many([Y|Ys], Xs, Zs) :-
+selectchk_many([], Xs, Xs).
+selectchk_many([Y|Ys], Xs, Zs) :-
   selectchk(Y, Xs, As),
-  select_many(Ys, As, Zs).
+  selectchk_many(Ys, As, Zs).
